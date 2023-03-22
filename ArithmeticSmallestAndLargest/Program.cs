701f8f6fa32efbe9a445dd74043e4953c5af9a40
AbdulRahmanMudasser <mrhprince7@gmail.com>
2023-03-23 04:06:58 +0500

    Add project files.

---
+++ ArithmeticSmallestAndLargest/Program.cs
@@ -0,0 +1,42 @@
+﻿class ArithmeticSmallestAndLargest
+{
+    static void Main(string[] args)
+    {
+        // declaring variables
+        int firstNumber;
+        int secondNumber;
+        int thirdNumber;
+
+        // prompting user and taking input for firstNumber
+        Console.Write("Enter First Number:  ");
+        firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        // prompting user and taking input for secondNumber
+        Console.Write("Enter Second Number:  ");
+        secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        // prompting user and taking input for thirdNumber
+        Console.Write("Enter Third Number:  ");
+        thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+        // arithmetic operations
+        int sum = firstNumber + secondNumber + thirdNumber;
+        int product = firstNumber * secondNumber * thirdNumber;
+        int average = (int)(sum / 3);
+
+        // comparing
+
+        Console.WriteLine();    // just for spacing purpose
+
+        // displaying sum of three numbers
+        Console.WriteLine("Sum Of Numbers:  " + sum);
+
+        // displaying product of three numbers
+        Console.WriteLine("Product Of Numbers:  " + product);
+
+        // displaying avearge of three numbers
+        Console.WriteLine("Average Of Numbers:  " + average);
+
+        Console.WriteLine();    // just for spacing purpose
+    }
+}
